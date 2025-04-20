246cf978e05a0842b74038744b2711697c9745f5
PREETHINRPREETHINR <preethinr07@gmail.com>
2025-04-20 10:30:46 -0400

    home link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,10 @@ import { AccountaddComponent } from './components/accountadd/accountadd.componen
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeComponent],
   template: `
     <!-- <h1>Welcome to {{title}}!</h1> -->
-    <!-- <app-home/> -->
+    <app-home/>
     <!-- <app-dashboard/>
     <app-accountadd/> -->
     <router-outlet />
